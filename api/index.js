b06e3145d865f6cfda58c4a5b3c881b7166e4e60
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-08-01 12:47:47 +0500

    connection to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to the db")
+}).catch((err)=>{
+    console.log(`db error : ${err}`)
+})
 
 const app = express()
 const port = process.env.PORT || 3000
