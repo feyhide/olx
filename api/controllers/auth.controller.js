7cadc580aac3b2e1fd402dd09de515fd1bafe88c
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-08-01 13:42:08 +0500

    hide password when sign in ((rest))

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -34,7 +34,8 @@ export const signin = async (req, res, next) => {
         }
 
         const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
-        res.cookie("access_token",token,{httpOnly:true}).status(200).json(validUser)
+        const {password: pass, ...rest} = validUser._doc
+        res.cookie("access_token",token,{httpOnly:true}).status(200).json(rest)
         
     } catch (error) {
         next(error);
