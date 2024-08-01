8860bbf6fa15a1bada2c5383d3d78ec0bc081762
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-08-01 13:05:19 +0500

    created routes and controller for checking with postman

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
@@ -10,9 +12,12 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 })
 
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 3000
-
 
 app.listen(port,()=>{
     console.log(`SERVER IS LISTENING ON PORT ${port}`)
 })
+
+app.use("/api/v1/user",userRouter)
+app.use("/api/v1/auth",authRouter)
